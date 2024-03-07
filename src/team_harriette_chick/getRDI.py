72c87d6cf506a91815f1487bf0f1da89e21c2dcb
chris-kasa <chris.nicholas@kasa.com>
2024-03-07 12:58:52 -0800

    linting

--- src/team_harriette_chick/getRDI.py
+++ src/team_harriette_chick/getRDI.py
@@ -6,6 +6,7 @@ DRIs = None
 with open('data/diet.pickle', 'rb') as fp:
     DRIs = pickle.load(fp)
 
+
 def getRDIColumns():
     """Get the columns of a returned RDI."""
     return ['Energy',
@@ -29,24 +30,26 @@ def getRDIColumns():
             'Vitamin K (phylloquinone)',
             'Zinc, Zn']
 
+
 def getBin(age: int):
     """Get the correct age string for our table."""
     # just brute force for now
     if (age > 0) and (age <= 3):
-        return('1-3')
+        return ('1-3')
     if (age > 3) and (age <= 8):
-        return('4-8')
+        return ('4-8')
     if (age > 8) and (age <= 13):
-        return('9-13')
+        return ('9-13')
     if (age > 13) and (age <= 18):
-        return('14-18')
+        return ('14-18')
     if (age > 18) and (age <= 30):
-        return('19-30')
+        return ('19-30')
     if (age > 30) and (age <= 50):
-        return('31-50')
+        return ('31-50')
     if (age > 50):
-        return('51+')
- 
+        return ('51+')
+
+
 def getRDI(age: int, sex: str):
     """Get the recommended daily requirement for an individual.
 
@@ -79,10 +82,10 @@ def getRDI(age: int, sex: str):
             Zinc, Zn
         """
     if (age < 1) or (age > 120):
-        raise ValueException("age must be valid int between 1 and 120")
+        raise ValueError("age must be valid int between 1 and 120")
 
     if sex.upper() not in ['C', 'M', 'F']:
-        raise ValueException(f'invalid sex {sex}')
+        raise ValueError(f'invalid sex {sex}')
 
     bin = getBin(age)
     if bin == '1-3':
